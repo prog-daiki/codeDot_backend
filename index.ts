6ab77b6cc317ddb923d89c78b01e4030840ae22b
prog-daiki <t.daiki0913@icloud.com>
2024-09-18 20:55:07 +0900

    portとhostnameの設定

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 8000,
+  hostname: "0.0.0.0",
+  fetch: (req) => {
+    return new Response("Hello via Bun!");
+  },
+});
+
+console.log("server running", server.port);
