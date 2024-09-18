bf9f75ad8150a45c18a30bcd84f3c9fc2266ef7c
prog-daiki <t.daiki0913@icloud.com>
2024-09-18 21:09:11 +0900

    honoのインストール

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
+import app from "./app";
+
 const server = Bun.serve({
   port: 8000,
   hostname: "0.0.0.0",
-  fetch: (req) => {
-    return new Response("Hello via Bun!");
-  },
+  fetch: app.fetch,
 });
 
 console.log("server running", server.port);
