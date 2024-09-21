58849c01c4a4d3a31a222848e75a0781547f9cd3
prog-daiki <t.daiki0913@icloud.com>
2024-09-21 23:01:31 +0900

    カテゴリーの存在チェックを作成

--- src/core/category/repository/index.ts
+++ src/core/category/repository/index.ts
@@ -1,4 +1,4 @@
-import { asc } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 import { db } from "../../../../db/drizzle";
 import { category } from "../../../../db/schema";
 import type { Category } from "../types";
@@ -14,5 +14,25 @@ export class CategoryRepository {
   async getCategories(): Promise<Category[]> {
     const data = await db.select().from(category).orderBy(asc(category.name));
     return data;
+  }
+
+  /**
+   * カテゴリーをIDで取得する
+   * @param id カテゴリーのID
+   * @returns {Promise<Category | null>} カテゴリーのオブジェクト
+   */
+  async getCategoryById(id: string): Promise<Category | null> {
+    const [data] = await db.select().from(category).where(eq(category.id, id));
+    return data;
+  }
+
+  /**
+   * カテゴリーが存在するかを確認する
+   * @param id カテゴリーのID
+   * @returns {Promise<boolean>} カテゴリーが存在するかどうか
+   */
+  async isCategoryExists(id: string): Promise<boolean> {
+    const category = await this.getCategoryById(id);
+    return !!category;
   }
 }
