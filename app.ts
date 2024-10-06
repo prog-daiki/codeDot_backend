701339aaf22586e64ab7f45b61a4c1073d6131b0
prog-daiki <t.daiki0913@icloud.com>
2024-10-06 23:51:07 +0900

    corsとcsrf設定を追加

--- app.ts
+++ app.ts
@@ -4,9 +4,29 @@ import Course from "./src/core/course";
 import { clerkMiddleware } from "@hono/clerk-auth";
 import Category from "./src/core/category";
 import Chapter from "./src/core/chapter";
+import { cors } from "hono/cors";
+import { csrf } from "hono/csrf";
 
 const app = new Hono();
-app.use("*", logger(), clerkMiddleware());
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:3000"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  }),
+  logger(),
+  clerkMiddleware(),
+);
+
+app.use(
+  "/api/*",
+  csrf({
+    origin: "http://localhost:3000",
+  }),
+);
+
 app
   .route("/api/courses", Course)
   .route("/api/categories", Category)
