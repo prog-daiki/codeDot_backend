a2e0f6c19c38c3ef174e6b7c0d621cf98d8feb4a
daikitakada <d-takada@zyyx.jp>
2024-10-15 12:46:59 +0900

    講座ソースコード編集APIのリファクタリング

--- src/core/course/index.ts
+++ src/core/course/index.ts
@@ -328,7 +328,7 @@ Course.put(
     const { course_id: courseId } = c.req.valid("param");
     const courseUseCase = c.get("courseUseCase");
     try {
-      const course = await courseUseCase.updateCourseSourceUrl(courseId, validatedData.sourceUrl);
+      const course: Course = await courseUseCase.updateCourseSourceUrl(courseId, validatedData.sourceUrl);
       return c.json(course);
     } catch (error) {
       if (error instanceof CourseNotFoundError) {
