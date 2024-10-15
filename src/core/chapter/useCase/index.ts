13a09e350e7c6e4645606933aaca8f404072d7c6
prog-daiki <t-daiki0913@icloud.com>
2024-10-15 12:14:43 +0900

    チャプター公開APIのリファクタリング

--- src/core/chapter/useCase/index.ts
+++ src/core/chapter/useCase/index.ts
@@ -177,7 +177,7 @@ export class ChapterUseCase {
    * @param courseId 講座ID
    * @param list チャプターリスト
    */
-  async reorderChapters(courseId: string, list: { id: string; position: number }[]) {
+  async reorderChapters(courseId: string, list: { id: string; position: number }[]): Promise<void> {
     // 講座の存在チェック
     const isCourseExists: boolean = await this.courseRepository.isCourseExists(courseId);
     if (!isCourseExists) {
@@ -198,7 +198,7 @@ export class ChapterUseCase {
    * @param courseId
    * @param chapterId
    */
-  async deleteChapter(courseId: string, chapterId: string) {
+  async deleteChapter(courseId: string, chapterId: string): Promise<Chapter> {
     // 講座の存在チェック
     const isCourseExists: boolean = await this.courseRepository.isCourseExists(courseId);
     if (!isCourseExists) {
@@ -239,7 +239,7 @@ export class ChapterUseCase {
    * @param courseId 講座ID
    * @param chapterId チャプターID
    */
-  async unpublishChapter(courseId: string, chapterId: string) {
+  async unpublishChapter(courseId: string, chapterId: string): Promise<Chapter> {
     // 講座の存在チェック
     const isCourseExists: boolean = await this.courseRepository.isCourseExists(courseId);
     if (!isCourseExists) {
@@ -271,27 +271,27 @@ export class ChapterUseCase {
    * @param courseId 講座ID
    * @param chapterId チャプターID
    */
-  async publishChapter(courseId: string, chapterId: string) {
+  async publishChapter(courseId: string, chapterId: string): Promise<Chapter> {
     // 講座の存在チェック
-    const isCourseExists = await this.courseRepository.isCourseExists(courseId);
+    const isCourseExists: boolean = await this.courseRepository.isCourseExists(courseId);
     if (!isCourseExists) {
       throw new CourseNotFoundError();
     }
 
     // チャプターの存在チェック
-    const isChapterExists = await this.chapterRepository.isChapterExists(chapterId);
+    const isChapterExists: boolean = await this.chapterRepository.isChapterExists(chapterId);
     if (!isChapterExists) {
       throw new ChapterNotFoundError();
     }
 
     // MuxDataの存在チェック
-    const existsMuxData = await this.muxDataRepository.checkMuxDataExists(chapterId);
+    const existsMuxData: MuxData | null = await this.muxDataRepository.checkMuxDataExists(chapterId);
     if (!existsMuxData) {
       throw new MuxDataNotFoundError();
     }
 
     // チャプターの必須フィールドが空かどうかを確認
-    const data = await this.chapterRepository.getChapterById(chapterId);
+    const data: ChapterWithMuxData = await this.chapterRepository.getChapterById(chapterId);
     if (!data.chapter.title || !data.chapter.description || !data.chapter.videoUrl) {
       throw new ChapterRequiredFieldsEmptyError();
     }
