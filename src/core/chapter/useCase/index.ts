e55460eaf742bc6977168e1fc5cbe9e5cd2ee2b0
daikitakada <d-takada@zyyx.jp>
2024-10-15 12:09:53 +0900

    チャプター非公開APIのリファクタリング

--- src/core/chapter/useCase/index.ts
+++ src/core/chapter/useCase/index.ts
@@ -241,23 +241,23 @@ export class ChapterUseCase {
    */
   async unpublishChapter(courseId: string, chapterId: string) {
     // 講座の存在チェック
-    const isCourseExists = await this.courseRepository.isCourseExists(courseId);
+    const isCourseExists: boolean = await this.courseRepository.isCourseExists(courseId);
     if (!isCourseExists) {
       throw new CourseNotFoundError();
     }
 
     // チャプターの存在チェック
-    const isChapterExists = await this.chapterRepository.isChapterExists(chapterId);
+    const isChapterExists: boolean = await this.chapterRepository.isChapterExists(chapterId);
     if (!isChapterExists) {
       throw new ChapterNotFoundError();
     }
 
-    const chapter = await this.chapterRepository.updateChapter(chapterId, {
+    const chapter: Chapter = await this.chapterRepository.updateChapter(chapterId, {
       publishFlag: false,
     });
 
     // 講座のチャプターが0件になった場合、講座を非公開にする
-    const chapters = await this.chapterRepository.getPublishChapters(courseId);
+    const chapters: Chapter[] = await this.chapterRepository.getPublishChapters(courseId);
     if (chapters.length === 0) {
       await this.courseRepository.updateCourse(courseId, {
         publishFlag: false,
