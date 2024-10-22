a646dc06dd78da8d80d72f2e67d6a42a4f66752b
prog-daiki <t.daiki0913@icloud.com>
2024-10-22 10:49:43 +0900

    webhookの追加

--- app.ts
+++ app.ts
@@ -3,9 +3,10 @@ import { logger } from "hono/logger";
 import Course from "./src/core/course";
 import { clerkMiddleware } from "@hono/clerk-auth";
 import Category from "./src/core/category";
-import Chapter from "./src/core/chapter";
 import { cors } from "hono/cors";
 import { csrf } from "hono/csrf";
+import Webhook from "./src/core/webhook";
+import Chapter from "./src/core/chapter";
 
 const app = new Hono();
 app.use(
@@ -30,6 +31,7 @@ app.use(
 app
   .route("/api/courses", Course)
   .route("/api/categories", Category)
-  .route("/api/courses/:course_id/chapters", Chapter);
+  .route("/api/courses/:course_id/chapters", Chapter)
+  .route("/webhook", Webhook);
 
 export default app;
